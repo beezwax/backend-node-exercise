ffaf7e25bccadfac71cc999ee646c272d0444140
Dan Bridges <bridges2@gmail.com>
2022-05-20 14:26:26 -0700

    Wip

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import knex from "knex";
 
+interface Transponder {
+  id: number;
+  name: string;
+  children: Transponder[];
+}
+
+interface TransponderWithParent {
+  id: number;
+  name: string;
+  parentId: number | null;
+}
+
 const db = knex({
   client: "sqlite3",
   connection: {
@@ -14,9 +26,68 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/count", async (_: Request, res: Response) => {
-  const count = await db("transponders").count("id as count");
-  res.json(count[0]);
+function toTransponder({ id, name }: TransponderWithParent): Transponder {
+  return { id, name, children: [] as Transponder[] };
+}
+
+function buildTransponders(
+  transponders: TransponderWithParent[]
+): Transponder[] {
+  const nodes = transponders
+    .filter((t) => t.parentId == null)
+    .map(toTransponder);
+
+  const rest = transponders.filter((t) => t.parentId != null);
+
+  const grouped = rest.reduce((acc, t) => {
+    if (t.parentId == null) return acc;
+
+    if (acc[t.parentId] == null) {
+      acc[t.parentId] = [t];
+    } else {
+      acc[t.parentId].push(t);
+    }
+
+    return acc;
+  }, {} as Record<number, TransponderWithParent[]>);
+
+  const buildTree = (t: Transponder): Transponder => {
+    t.children = (grouped[t.id] ?? []).map((t) => buildTree(toTransponder(t)));
+
+    return t;
+  };
+
+  return nodes.map(buildTree);
+}
+
+app.get("/transponders", async (_: Request, res: Response) => {
+  const transponders = await db
+    .select(
+      "transponders.id",
+      "transponders.name",
+      "transponder_relations.parentId"
+    )
+    .from("transponders")
+    .leftJoin(
+      "transponder_relations",
+      "transponders.id",
+      "transponder_relations.childId"
+    );
+  res.json(buildTransponders(transponders));
+});
+
+app.get("/count", async (req: Request, res: Response) => {
+  if (req.query.id != null) {
+    const count = await db("transponder_relations")
+      .where({
+        parentId: req.query.id,
+      })
+      .count("id as count");
+    res.json(count[0]);
+  } else {
+    const count = await db("transponders").count("id as count");
+    res.json(count[0]);
+  }
 });
 
 app.listen(port, () => {
